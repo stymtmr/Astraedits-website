b2e15333b1517b053c0998137ad1cadbf269414c
Satyam Tomar <team-astra@Satyams-MacBook-Pro.local>
2025-12-20 03:55:07 +0530

    Warning update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['via.placeholder.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
